61d282b8849bb8e124e169adff5894582cc7387c
Jeff Beck <jeff.kitconcepts@gmail.com>
2015-02-10 07:49:34 -0500

    minor adjustments for format and added a 0 in front of my hour.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var counter = function() {
   var hour = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
+  if(hour.toString().length < 2) {
+                  hour = "0" + hour;
+}
   if(minutes.toString().length < 2) {
                   minutes = "0" + minutes;
 }
@@ -21,6 +24,19 @@ var counter = function() {
                  "color" : "white",
       "background-color" : "black"
   })
+  var colorString = "#" + hour + minutes + seconds;
+
+  $("span").text(colorString).css({
+                "dislay" : "block",
+            "text-align" : "center",
+             "font-size" : "80px",
+            "font-style" : "italic",
+           "padding-top" : "150px",
+                "height" : "300px",
+                 "width" : "600px",
+                 "color" : "white",
+      "background-color" : colorString
+  })    
 }
 var intID = setInterval(counter, 1000);
 $(function(){})
